9844b7a9cf9b6627b82341a0647b8559a2ff760d
Yash Saxena <44087192+yashsaxena003@users.noreply.github.com>
2022-10-05 18:28:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const io = require("socket.io")(server, {
 		origin: "*",
 		methods: [ "GET", "POST" ]
 	}
-});
+}
+			       );
 
 app.use(cors());
 
@@ -15,22 +16,27 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send('Running');
-});
+}
+       );
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
 
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
-	});
+	}
+		 );
 
 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
+	}
+		 );
 
 	socket.on("answerCall", (data) => {
 		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
+	}
+		 );
+}
+     );
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
